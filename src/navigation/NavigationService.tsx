8de04f66e9f3380ca4ce7d4e0fc0b0f014a0183e
yban <laubana@gmail.com>
2024-02-07 11:07:20 -0800

    add user avatar

--- src/navigation/NavigationService.tsx
+++ src/navigation/NavigationService.tsx
@@ -3,6 +3,7 @@ import { NavigationContainerRef } from "@react-navigation/native";
 
 type Collections = {
   // PLOP_COMPONENT_TYPE
+ UserAvatarCollection: undefined;
   DateTimeSelectorCollection: undefined;
   CheckboxCollection: undefined;
   TestModuleCollection: undefined;
--- src/navigation/NavigationStack.tsx
+++ src/navigation/NavigationStack.tsx
@@ -9,6 +9,7 @@ import SampleScreen from "../screens/SampleScreen";
 import BottomNavigation from "../components/layout/BottomNavigation";
 
 // PLOP_INJECT_COLLECTION_IMPORT
+import UserAvatarCollection from '../collections/base/UserAvatar'
 import DateTimeSelectorCollection from "../collections/base/DateTimeSelector";
 import CheckboxCollection from "../collections/base/Checkbox";
 
@@ -24,6 +25,7 @@ const PrivateStack = createStackNavigator();
 const PrivateNavigator = () => {
   const components = [
     // PLOP_INJECT_NAVIGATOR_SCREEN
+{func: UserAvatarCollection, custom: false},
     { func: DateTimeSelectorCollection, custom: false },
     { func: CheckboxCollection, custom: false },
     { func: SampleScreen, custom: false },
