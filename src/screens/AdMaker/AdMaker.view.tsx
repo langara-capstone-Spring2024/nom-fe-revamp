8b7f870538d6e0e9a153476c3d70ca3f9ada4f63
Don Cosio <dacosio@gmail.com>
2024-02-23 19:35:10 -0800

    page 2

--- src/screens/AdMaker/AdMaker.view.tsx
+++ src/screens/AdMaker/AdMaker.view.tsx
@@ -15,11 +15,35 @@ const AdMaker = (props: AdMakerGeneratedProps) => {
 
   const theme = useTheme();
   const styles = useMemo(() => createStyles(theme as any), [theme]);
+  const content = () => {
+    if (page === 1) {
+      return (
+        <>
+          <Typography variant="title5" alignment="left" color="primary">
+            Upload Ad Image
+          </Typography>
+          <Typography variant="bodySm" alignment="left" color="primary">
+            Recommended image ratio is 2:1
+          </Typography>
+        </>
+      );
+    } else if (page === 2) {
+      return (
+        <>
+          <Typography variant="title5" alignment="left" color="primary">
+            Select a Template
+          </Typography>
+          {/* Add more JSX for page 2 */}
+        </>
+      );
+    }
+    // Add more conditions for other pages if needed
+  };
 
   return (
     <View style={styles.container}>
       <Progress.Bar
-        progress={page/4}
+        progress={page / 4}
         color={t.Border.contrast}
         unfilledColor={"#D4D4D4"}
         borderWidth={0}
@@ -29,21 +53,24 @@ const AdMaker = (props: AdMakerGeneratedProps) => {
       />
       <View style={styles.wrapper}>
         <View style={styles.contentContainer}>
-          <CircularNumber number={1} />
-          <View style={styles.textContainer}>
-            <Typography variant="title5" alignment="left" color="primary">
-              Upload Ad Image
-            </Typography>
-            <Typography variant="bodySm" alignment="left" color="primary">
-              Recommended image ratio is 2:1
-            </Typography>
-          </View>
+          <CircularNumber number={page} />
+          <View style={styles.textContainer}>{content()}</View>
         </View>
       </View>
-      <View style={styles.imagePicker}>
-        <AdImagePicker image={localImage} setImage={handleImageChange} />
-      </View>
+      {page === 1 && (
+        <View style={styles.imagePicker}>
+          <AdImagePicker image={localImage} setImage={handleImageChange} />
+        </View>
+      )}
       <View style={styles.buttonContainer}>
+        <Button
+          variant="primary"
+          buttonSize="lg"
+          text="Previous"
+          takeFullWidth
+          onPress={prev}
+          isDisabled={!localImage}
+        />
         <Button
           variant="primary"
           buttonSize="lg"
