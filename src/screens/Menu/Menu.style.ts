c668f5b1c61fecdb74bc3fa860ef744214f0ae3d
hcheng40@mylangara.ca <>
2024-03-01 20:31:30 -0800

    fix bug

--- src/screens/Menu/Menu.style.ts
+++ src/screens/Menu/Menu.style.ts
@@ -3,15 +3,6 @@ import { Theme } from "../../config/theme-config";
 
 const createStyles = (theme: Theme) =>
   StyleSheet.create({
-    container: {},
-  });
-
-export default createStyles;
-import { StyleSheet } from "react-native";
-import { Theme } from "../../config/theme-config";
-
-const createStyles = (theme: Theme) =>
-  StyleSheet.create({
     container: {
       backgroundColor: "#fff",
     },
