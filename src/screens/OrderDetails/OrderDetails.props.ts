d9a6f3c43117c2a667edaf58350f4370f619290a
hcheng40@mylangara.ca <>
2024-03-13 18:02:19 -0700

    BE: OrderDetails

--- src/screens/OrderDetails/OrderDetails.props.ts
+++ src/screens/OrderDetails/OrderDetails.props.ts
@@ -8,4 +8,19 @@ export interface OrderDetailsGeneratedProps {
   setOpenSuccess: (v: boolean) => void;
   modalVisible: boolean;
   setModalVisible: (v: boolean) => void;
+  customerName: String, 
+  couponNo: string,
+  discount: number, 
+  date: Date, 
+  validFromTime: Date, 
+  validToTime: Date,
+  status: String, 
+  operation: Date,
+  menuId: string;
+  menuImage: string;
+  menuName: string;
+  menuPrice: number;
+  selected?: boolean;
+  handleSelect: (menuId: string) => void;
+  hideRadioButton: boolean;
 }
--- src/services/client.ts
+++ src/services/client.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const apiClient = axios.create({
-  baseURL: process.env.BACKEND_URL || "http://localhost:8000/",
+  baseURL: "http://192.168.1.86:8000/",
+  // process.env.BACKEND_URL || "http://localhost:8000/",
   responseType: "json",
   withCredentials: true,
 });
