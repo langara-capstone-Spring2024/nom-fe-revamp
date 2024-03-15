c4516a4527f74dc4f7c889e1aeb6cb9440eed2d2
laubana <45812484+laubana@users.noreply.github.com>
2024-03-15 12:44:00 -0700

    change bug, style (#90)

--- src/components/base/MultipleImagePicker/MultipleImagePicker.style.ts
+++ src/components/base/MultipleImagePicker/MultipleImagePicker.style.ts
@@ -1,33 +1,51 @@
 import { StyleSheet } from "react-native";
+import { Theme } from "../../../config/theme-config";
+import { theme as t } from "../../../utils/Theme";
 
 const styles = StyleSheet.create({
-  container: {
+  listContainer: {
+    marginTop: 8,
     width: "100%",
     flexWrap: "wrap",
     flexDirection: "row",
     justifyContent: "flex-start",
-    margin: -4,
-  },
-  item: {
-    width: "25%",
-    height: "100%",
-    borderRadius: 8,
-    aspectRatio: 1,
-    padding: 4,
+    gap: 8,
   },
   button: {
     width: "100%",
-    height: "100%",
+    aspectRatio: 1.8,
+    borderRadius: 16,
+    backgroundColor: t.Surface.raised,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
     borderRadius: 8,
-    backgroundColor: "gray",
+    aspectRatio: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   image: {
     width: "100%",
     height: "100%",
-    borderRadius: 8,
     objectFit: "cover",
+  },
+  background: {
+    position: "absolute",
+    borderRadius: 8,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+  },
+  error: {
+    marginTop: 8,
+    paddingHorizontal: 8,
   },
 });
 
