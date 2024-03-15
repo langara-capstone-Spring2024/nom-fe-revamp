34cf7cefc53e0b1f23abc483b725194c308bdd8e
dacosio <dacosio@gmail.com>
2024-03-15 12:42:49 -0700

    [For Review]Cap 295 fe continue with all the templates (#88)
    * whole flow except the bloced template design
    
    * removed default
    
    * v1
    
    * completed template one
    
    * second template
    
    * container
    
    * template
    
    * git add .
    removed third template
    
    * completed

--- src/components/base/AdTemplateThree/AdTemplateThree.style.ts
+++ src/components/base/AdTemplateThree/AdTemplateThree.style.ts
@@ -2,8 +2,54 @@ import { StyleSheet } from "react-native";
 import { Theme } from "../../../config/theme-config";
 import { theme as t } from "../../../utils/Theme";
 
-const createStyles = (theme: Theme) => StyleSheet.create({
-    
-});
+const createStyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      position: "relative",
+      width: "100%",
+      height: "100%",
+      borderRadius: 24,
+      overflow: "hidden",
+    },
+    image: {
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+    primary: {
+      // width: "100%",
+      // height: "100%",
+      // marginTop: 15,
+      // marginLeft: 28,
+      // zIndex: 1,
+    },
+    secondary: {
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      zIndex: 0,
+      marginLeft: -10,
+    },
+    headline: {
+      position: "absolute",
+      top: "20%",
+      left: "16%",
+      width: 120,
+      zIndex: 2,
+      fontFamily: "PublicSansBold",
+      color: "#3C3C3C",
+      fontSize: 19,
+    },
+    tagline: {
+      position: "absolute",
+      top: "55%",
+      left: "16%",
+      width: 120,
+      zIndex: 2,
+      color: "#3C3C3C",
+    },
+  });
 
 export default createStyles;
--- src/components/base/AdTemplateTwo/AdTemplateTwo.props.ts
+++ src/components/base/AdTemplateTwo/AdTemplateTwo.props.ts
@@ -1,12 +1,14 @@
+import { TextStyle, ViewStyle } from "react-native";
 import { Image } from "../../../types";
 
 export interface AdTemplateTwoProps {
   image?: Image;
   headline: string;
   tagline: string;
-  variant: number;
   primary: string;
   secondary: string;
   width?: string;
   height?: string;
+  style?: TextStyle & ViewStyle;
+  onSelectTemplate: (v: number) => void;
 }
