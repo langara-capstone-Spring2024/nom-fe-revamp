6ba759b1176bfb05bf04ce61f0fbb409035e97d2
hcheng40@mylangara.ca <>
2024-03-19 23:11:53 -0700

    restore default value

--- src/services/client.ts
+++ src/services/client.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const apiClient = axios.create({
-  baseURL: "http://192.168.1.86:8000/",
-  // process.env.BACKEND_URL || "http://localhost:8000/",
+  baseURL: process.env.BACKEND_URL || "http://localhost:8000/",
   responseType: "json",
   withCredentials: true,
 });
