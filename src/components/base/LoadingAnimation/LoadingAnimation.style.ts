bb7cc1146040f757120055d42b406897d3208185
hchenghy <134082406+hchenghy@users.noreply.github.com>
2024-03-30 14:03:12 -0700

    [For Review] Loading animation v2 (#130)
    * fix scanner bug
    
    * success modal check color fix
    
    * remove testing code
    
    * add nom loading animation
    
    * restore default value
    
    * restore loading animation
    
    * nom loading animation
    
    * backup loading animation
    
    ---------
    
    Co-authored-by: hcheng40@mylangara.ca <>

--- src/components/base/LoadingAnimation/LoadingAnimation.style.ts
+++ src/components/base/LoadingAnimation/LoadingAnimation.style.ts
@@ -2,22 +2,18 @@ import { StyleSheet, Dimensions } from "react-native";
 import { Theme } from "../../../config/theme-config";
 import { theme as t } from "../../../utils/Theme";
 
-const createStyles = (theme: Theme) => StyleSheet.create({
+const createStyles = (theme: Theme) =>
+  StyleSheet.create({
     container: {
-        // flex: 1,
-        // flexDirection:'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        // margin: 'auto',
-        // backgroundColor: 'pink',
-        height: '100%'
-      },
-    
-      animation: {
-        width: Dimensions.get('window').width * 0.5,
-        height: Dimensions.get('window').width * 0.5,
-      },
-    
-    });
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100%",
+    },
+
+    animation: {
+      width: Dimensions.get("window").width * 0.9,
+      height: Dimensions.get("window").width * 0.9,
+    },
+  });
 
 export default createStyles;
